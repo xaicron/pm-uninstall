86ed739c935e22f9764c79f0e6ba18edc1d6a75a
xaicron <xaicron@gmail.com>
2010-03-10 23:26:24 +0900

    initial commit

---
+++ lib/App/moduleuninstall.pm
@@ -0,0 +1,34 @@
+package App::moduleuninstall;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::moduleuninstall -
+
+=head1 SYNOPSIS
+
+  use App::moduleuninstall;
+
+=head1 DESCRIPTION
+
+App::moduleuninstall is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
