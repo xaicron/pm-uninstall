06a38decc1fff4585e95f3ebfdf75c4349d655db
xaicron <xaicron@gmail.com>
2010-03-12 00:17:15 +0900

    first commit

--- lib/App/pmuninstall.pm
+++ lib/App/pmuninstall.pm
@@ -1,4 +1,4 @@
-package App::moduleuninstall;
+package App::pmuninstall;
 use strict;
 use warnings;
 our $VERSION = '0.01';
@@ -8,17 +8,12 @@ __END__
 
 =head1 NAME
 
-App::moduleuninstall -
-
-=head1 SYNOPSIS
-
-  use App::moduleuninstall;
+App::pmuninstall - Uninstall modules
 
 =head1 DESCRIPTION
 
-App::moduleuninstall is
-
-=head1 METHODS
+App::pmuninstall is Fast module uninstaller.
+delete files from B<.packlist>.
 
 =head1 AUTHOR
 
@@ -26,9 +21,12 @@ Yuji Shimada E<lt>xaicron@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
+L<pm-uninstall>
+
 =head1 LICENSE
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
