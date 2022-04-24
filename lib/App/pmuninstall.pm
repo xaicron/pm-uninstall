cb78024b594b015caf3c03ec5907225d8ac98b0b
Yuji Shimada <xaicron@gmail.com>
2022-04-25 03:17:04 +0900

    Revert "removed probably unnecessary code"
    This reverts commit 00f3cc191cef64bc3814b7307bf61370222e740b.

--- lib/App/pmuninstall.pm
+++ lib/App/pmuninstall.pm
@@ -308,7 +308,8 @@ sub is_local_lib {
     my ($self, $file) = @_;
     return unless $self->{local_lib};
 
-    my $local_lib_base = quotemeta Cwd::realpath($self->{local_lib});
+    my $local_lib_base = quotemeta File::Spec->canonpath(Cwd::realpath($self->{local_lib}));
+    $file = File::Spec->canonpath($file);
     $file = Cwd::realpath($file);
 
     return $file =~ /^$local_lib_base/ ? 1 : 0;
